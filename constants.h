9a3a9c9ee791a7e94d4e68059f8869b813690dc8
artie37 <artieknapp@dyn-169-226-64-45.wireless.albany.edu>
2014-05-08 17:35:15 -0400

    Contains the integer constants used in the program.

---
+++ constants.h
@@ -0,0 +1,24 @@
+// constants.h
+// Arthur Knapp
+// AK146316
+
+#define BIG_NUMBER 1000
+// Size of line to be read in
+#define START_POINT 3
+// Index value to start reading from argv
+#define ARCHIVE_INDEX 2
+// Index value of the archive file
+#define NOT_OPENED -1
+// Returned when stats could not be obtained from the file
+#define FILE_PERMISSION 0664
+// Octal value of the file permission when creating a file
+#define CONTENT_SIZE 100
+// Size of content charcter array
+#define FOUR_BYTES 4
+// Four bytes in the archive file
+#define SECOND_INDEX 2
+// Position 2 in argv
+#define THIRD_INDEX 3
+// Position 3 in argv
+#define MAX_FILES 255
+// Maximum amount of files

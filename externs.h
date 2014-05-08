6c13ed833ab7263c49692b9138b8efb940879dc3
artie37 <artieknapp@dyn-169-226-64-45.wireless.albany.edu>
2014-05-08 17:32:23 -0400

    Stores the extern variables

---
+++ externs.h
@@ -0,0 +1,11 @@
+// externs.h
+// Arthur Knapp
+// AK146316
+
+// File: functions.c~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// create---------------------------------------------------------------------------
+
+//char *argv;
+int argc;
+// Stores the number of arguments
